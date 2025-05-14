7073b76f96259202ce5546ea26346d81f9590089
admin <wyjnug411@gmail.com>
2025-05-14 10:47:15 +0900

    11주차 토큰큰

--- js/login.js
+++ js/login.js
@@ -65,7 +65,18 @@ const check_input = () => {
   const sanitizedEmail = check_xss(emailValue);
   // check_xss 함수로 비밀번호 Sanitize
 
-  const idsave_check = document.getElementById('idSaveCheck');
+const idsave_check = document.getElementById('idSaveCheck');
+const payload = {
+  id: emailValue,
+  exp: Math.floor(Date.now() / 1000) + 3600 // 1시간 (3600초)
+};
+const jwtToken = generateJWT(payload);
+
+
+
+
+
+
 
 
   if (emailValue === '') {
@@ -126,7 +137,18 @@ if(idsave_check.checked == true) { // 아이디 체크 o
   console.log('비밀번호:', passwordValue);
 
   session_set(); // 세션 생성
+  localStorage.setItem('jwt_token', jwtToken);
   loginForm.submit();
 };
 
 document.getElementById("login_btn").addEventListener('click', check_input);
+
+function init_logined(){
+if(sessionStorage){
+decrypt_text(); // 복호화 함수
+}
+else{
+alert("세션 스토리지 지원 x");
+}
+}
+
